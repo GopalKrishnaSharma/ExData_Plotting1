f0d335a0365e0e914c7e5aa3cad12e564b011a3d
GopalKrishnaSharma <gopal.krishnasharma@gmail.com>
2017-02-11 16:44:41 +0530

    plot1 code and output

---
+++ plot1.R
@@ -0,0 +1,10 @@
+##Three step code to generate the required plot
+
+#Loading data from the Electric Power Consumption dataset
+power <- read.table("household_power_consumption.txt",sep=";", header = TRUE)
+
+#Subsetting data to filter out records between 2007-02-01 and 2007-02-02
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+#Plotting the histogram and annotating the graph
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
